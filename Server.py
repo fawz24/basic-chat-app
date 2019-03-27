2fbaef881b8e429f0d307c2cf90ef98ae6967b51
fawzy abderrahman <fawzyabderrahman@gmail.com>
2019-03-27 15:45:33 +0300

    add index.html page

--- Server.py
+++ Server.py
@@ -6,10 +6,4 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to the chat app!'
-
-
-#with app.test_request_context():
-#    print(flask.url_for('hello_world'))
-#    print(flask.url_for('show_path', subpath=r'c:\test', redirect='home'))
-#    print(flask.url_for('projects'))
+    return flask.render_template('index.html')
